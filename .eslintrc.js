fd3a6df5b50307e3831936e746c8633c17f75474
joker <le_yemeng@163.com>
2021-11-03 17:14:50 +0800

    feat: eslint+prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true
-    },
-    extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
-    parserOptions: {
-      parser: '@babel/eslint-parser',
-      sourceType: "module",
-      ecmaVersion: 8,
-      requireConfigFile : false
-    },
-    rules: {
-      'no-console': 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      "vue/no-multiple-template-root": 0
-    }
-};
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    // 'plugin:@typescript-eslint/recommended',
+    'airbnb-base',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+
+  parserOptions: {
+    ecmaVersion: 13,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-unresolved': 0,
+    'vue/no-multiple-template-root': 0
+  }
+}
