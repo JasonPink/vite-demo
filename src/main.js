e6a07aacf57d37ce79544ebccc87592d5863798f
joker <le_yemeng@163.com>
2021-11-03 11:15:05 +0800

    12323

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router/index.ts'
 import store from '@/store/index.ts'
-
 import './style/index.scss'
 
 createApp(App).use(router).use(store).mount('#app')
